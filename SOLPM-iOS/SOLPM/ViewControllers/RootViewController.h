387973c953af8ba6d7ba6c1271bfc39fe7b743bf
Ryan Badilla <pickledprotoast2@gmail.com>
2016-05-19 15:15:16 -0400

    initial refactor push

---
+++ SOLPM-iOS/SOLPM/ViewControllers/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  SOLPM
+//
+//  Created by Ryan Badilla on 5/14/16.
+//  Copyright © 2016 Newdesto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ SOLPM-iOS/SOLPM/ViewControllers/RootViewController.m
@@ -0,0 +1,48 @@
+//
+//  RootViewController.m
+//  SOLPM
+//
+//  Created by Ryan Badilla on 5/14/16.
+//  Copyright © 2016 Newdesto. All rights reserved.
+//
+
+#import "RootViewController.h"
+#import "PowerMoveListViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+
+#pragma mark -
+#pragma mark - View Controller lifecycle
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+
+#pragma mark -
+#pragma mark - 
+
+#pragma mark -
+#pragma mark - didReceiveMemoryWarning
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
